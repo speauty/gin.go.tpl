cf422173331e1f85b621506be48f8b3e094dc39b
speauty <speauty@163.com>
2022-05-31 15:04:40 +0800

    增加日志;

--- lib/log/log.go
+++ lib/log/log.go
@@ -1,19 +1,60 @@
 package log
 
-import "github.com/sirupsen/logrus"
+import (
+	"gin.go.tpl/lib/config"
+	"github.com/sirupsen/logrus"
+	"sync"
+)
 
-// Log log
-// @todo waiting to complete
+var (
+	LogAPI  *Log
+	LogOnce sync.Once
+)
+
 type Log struct{}
 
+func NewLogAPI(config config.LogConf) *Log {
+	LogOnce.Do(func() {
+		LogAPI = &Log{}
+		if config.Level < 7 {
+			LogAPI.SetLevel(logrus.Level(config.Level))
+		}
+	})
+	return LogAPI
+}
+
+func (l Log) SetLevel(level logrus.Level) {
+	logrus.SetLevel(level)
+}
+
 func (l Log) Print(args ...interface{}) {
-	logrus.Print(args)
+	logrus.Println(args)
+}
+
+func (l Log) Trace(args ...interface{}) {
+	logrus.Traceln(args)
+}
+
+func (l Log) Debug(args ...interface{}) {
+	logrus.Debugln(args)
 }
 
 func (l Log) Info(args ...interface{}) {
-	logrus.Info(args)
+	logrus.Infoln(args)
+}
+
+func (l Log) Warn(args ...interface{}) {
+	logrus.Warnln(args)
 }
 
 func (l Log) Error(args ...interface{}) {
-	logrus.Error(args)
+	logrus.Errorln(args)
+}
+
+func (l Log) Fatal(args ...interface{}) {
+	logrus.Fatalln(args)
+}
+
+func (l Log) Panic(args ...interface{}) {
+	logrus.Panicln(args)
 }
