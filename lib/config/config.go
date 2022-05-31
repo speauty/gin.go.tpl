0bb7578356aa1e8aecadac0defef5d761b8d6136
speauty <speauty@163.com>
2022-05-31 15:04:09 +0800

    完善各种配置;

--- lib/config/config.go
+++ lib/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"fmt"
+	"gin.go.tpl/lib/constant"
 	"github.com/spf13/viper"
 )
 
@@ -10,19 +11,60 @@ type GlobalConf struct {
 	DBDriver string `mapstructure:"db_driver"`
 }
 
+type GinConf struct {
+	Mode string `mapstructure:"mode"`
+}
+
+type LogConf struct {
+	Level uint32 `mapstructure:"level"`
+}
+
+type ServerConf struct {
+	Protocol string  `mapstructure:"protocol"`
+	Domain   string  `mapstructure:"domain"`
+	Port     string  `mapstructure:"port"`
+	SSL      SSLConf `mapstructure:"ssl"`
+}
+
+func (sc ServerConf) GetAddr() string {
+	addr := sc.Domain
+	if sc.Port != "" {
+		addr += ":" + sc.Port
+	}
+	return addr
+}
+
+func (sc ServerConf) IsHttp() bool {
+	return sc.Protocol == constant.ProtocolHttp
+}
+
+func (sc ServerConf) IsHttps() bool {
+	return sc.Protocol == constant.ProtocolHttps
+}
+
+type SSLConf struct {
+	Certificate    string `mapstructure:"certificate"`
+	CertificateKey string `mapstructure:"certificate_key"`
+}
+
 type PgSqlConf struct {
 	Source string `mapstructure:"source"`
 }
 
 type RedisConf struct {
-	Host string `mapstructure:"host"`
-	Port string `mapstructure:"port"`
-	Auth string `mapstructure:"auth"`
-	DB   string `mapstructure:"db"`
+	Host      string `mapstructure:"host"`
+	Port      string `mapstructure:"port"`
+	Auth      string `mapstructure:"auth"`
+	DB        string `mapstructure:"db"`
+	MaxIdle   int    `mapstructure:"max_idle"`
+	MaxActive int    `mapstructure:"max_active"`
 }
 
 type Config struct {
 	Global GlobalConf `mapstructure:"global"`
+	Gin    GinConf    `mapstructure:"gin"`
+	Log    LogConf    `mapstructure:"log"`
+	Server ServerConf `mapstructure:"server"`
 	PgSql  PgSqlConf  `mapstructure:"pgsql"`
 	Redis  RedisConf  `mapstructure:"redis"`
 }
