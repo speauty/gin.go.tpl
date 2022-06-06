40ec37d0374cc5b8644305082399f44f85904319
speauty <speauty@163.com>
2022-06-06 17:57:41 +0800

    将日志中间件改为异步投递;

--- middleware/log.go
+++ middleware/log.go
@@ -10,11 +10,14 @@ type LogMiddleware struct{}
 
 func (lm LogMiddleware) GoThrough() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		log.LogAPI.GetLogger().WithFields(logrus.Fields{
-			"url": c.Request.RequestURI, "method": c.Request.Method,
-			"client": c.ClientIP(), "user-agent": c.Request.UserAgent(),
-			"referer": c.Request.Referer(), "host": c.Request.Host,
-		}).Info("access.log")
+		// 调整成异步处理
+		go func() {
+			log.LogAPI.GetLogger().WithFields(logrus.Fields{
+				"url": c.Request.RequestURI, "method": c.Request.Method,
+				"client": c.ClientIP(), "user-agent": c.Request.UserAgent(),
+				"referer": c.Request.Referer(), "host": c.Request.Host,
+			}).Info("access.log")
+		}()
 		c.Next()
 	}
 }
