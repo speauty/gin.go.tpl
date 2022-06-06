d9190216ffadb16b78d67eb8c412f0a9dfc7f8f1
speauty <speauty@163.com>
2022-06-06 14:43:07 +0800

    定义逻辑错误类;

---
+++ lib/errors/error.go
@@ -0,0 +1,43 @@
+package errors
+
+import "gin.go.tpl/lib/code"
+
+type Error interface {
+	Error() string
+	GetRealError() error
+	GetCode() code.Code
+}
+
+type BaseError struct {
+	code  code.Code
+	msg   string
+	cause error
+}
+
+func (be BaseError) Error() string {
+	return be.msg
+}
+
+func (be BaseError) GetCode() code.Code {
+	return be.code
+}
+
+func (be BaseError) GetRealError() error {
+	return be.cause
+}
+
+func (be BaseError) GenFromStdError(err error) Error {
+	return BaseError{code: code.StdErr, msg: err.Error(), cause: err}
+}
+
+func (be BaseError) GenFromMsg(msg string, cause error) Error {
+	return BaseError{code: code.StdErr, msg: msg, cause: cause}
+}
+
+func (be BaseError) GenFromCode(tmpCode code.Code, cause error) Error {
+	return BaseError{code: tmpCode, msg: tmpCode.GetMsg(), cause: cause}
+}
+
+func (be BaseError) GenFromComplete(tmpCode code.Code, errMsg string, cause error) Error {
+	return BaseError{code: tmpCode, msg: errMsg, cause: cause}
+}
---
+++ lib/errors/logic.go
@@ -0,0 +1,31 @@
+package errors
+
+import "gin.go.tpl/lib/code"
+
+type LogicError struct {
+	BaseError
+}
+
+func (le LogicError) GenFromStdError(err error) Error {
+	return LogicError{BaseError{code: code.StdErr, msg: err.Error(), cause: err}}
+}
+
+func (le LogicError) GenFromMsg(msg string, cause error) Error {
+	return LogicError{BaseError{code: code.StdErr, msg: msg, cause: cause}}
+}
+
+func (le LogicError) GenFromCode(tmpCode code.Code, cause error) Error {
+	return LogicError{BaseError{code: tmpCode, msg: tmpCode.GetMsg(), cause: cause}}
+}
+
+func (le LogicError) GenFromComplete(tmpCode code.Code, errMsg string, cause error) Error {
+	return LogicError{BaseError{code: tmpCode, msg: errMsg, cause: cause}}
+}
+
+func (le LogicError) Unique(cause error) Error {
+	return le.GenFromCode(code.StdDbUnique, cause)
+}
+
+func (le LogicError) NotFound(cause error) Error {
+	return le.GenFromCode(code.StdDbNotFound, cause)
+}
