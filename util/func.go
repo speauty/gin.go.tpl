07cacf91ea4d8a898adeacd5a0bd966f74d742d0
speauty <speauty@163.com>
2022-06-06 17:53:01 +0800

    增加生成随机字符串函数GenRandomStr;

--- util/func.go
+++ util/func.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"crypto/rand"
 	"crypto/sha1"
 	"encoding/hex"
 	"os"
@@ -17,9 +18,26 @@ func WaitForExit() {
 
 // GenStrEncodedAndSalt 对指定字符串进行加密, 返回加密后的字符串和盐
 func GenStrEncodedAndSalt(str *string) (string, string) {
-	salt := "static salt"
+	salt := GenRandomStr(32)
 	hash := sha1.New()
 	hash.Write([]byte(*str + salt))
 	credential := hex.EncodeToString(hash.Sum(nil))
 	return credential, salt
+}
+
+const alphaNumMap = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+// GenRandomStr 生成指定长度的随机字符串
+func GenRandomStr(length int) string {
+	var bytes = make([]byte, 2*length)
+	var outBytes = make([]byte, length)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		panic(err)
+	}
+	mapLen := len(alphaNumMap)
+	for i := 0; i < length; i++ {
+		outBytes[i] = alphaNumMap[(int(bytes[2*i])*256+int(bytes[2*i+1]))%(mapLen)]
+	}
+	return string(outBytes)
 }
